2f0e5dc8a5b58ebfcb13bd6ff8aeb380def0723e
LhaseParth2610 <parthlhase49.com>
2025-05-22 21:07:20 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.NODE_ENV === "production" ? "/visual-web-craft-studio" : "/"}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
